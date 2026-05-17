8f9595944aff4d9056f430a6a3ca1ab8392b4f41
abdurazzaaqabdallaa-svg <abdurazzaaqabdallaa@gmail.com>
2026-05-18 02:43:11 +0300

    Add health guidelines and remedies in Afaan Oromoo
    Added health-related content and guidelines in Afaan Oromoo, including benefits and usage instructions for various natural remedies.

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,300 @@
 
+Xib-un-Nabawi
+Appi Fayyaa
+
+Xib-un-Nabawi
+Appi
+Fayyaa
+
+1
+Faayidaalee Haba-suudaa
+4
+Faayidaa Qullubbii Adii
+5
+Faayidaalee Hulubataa
+6
+Faayidaa Qoonxar/Talbaa
+7
+Faayidaa Shunkurtaa Diimaa
+8
+Faayidaalee Rummaanaa
+9
+Faayidaalee Timiraa
+10
+Faayidaalee Tamiirul Hindii
+11
+Faayidaalee Jinjiibilaa
+12
+Faayidaalee Qaranfulii
+13
+Faayidaalee Quusxiul Hindii
+14
+Faayidaalee Dhadhaa Sa'aa
+15
+Faayidaalee Aannanii
+16
+Faayidaalee Qarfaatii
+17
+Faayidaalee Kaarotii
+18
+Wantoota Fayyaa Miidhan
+19
+Eegumsa fi Guddina Daa'immanii
+20
+Qoricha Fayyaa Dubartootaa
+21
+Eegumsa Fayyaa Dubartii Ulfaa
+22
+Qoricha Dubartii Deesseef
+23
+Eegumsa Fayyaa Dargaggootaaf
+24
+Eegumsa Fayyaa Maanguddootaaf
+25
+Ilaalcha Isportii
+26
+Qajeelfama Uffataa
+27
+Qajeelfama Dhugaatii
+28
+Qajeelfama Hirribaa
+29
+Irraanfannaa fi Yaadannoo
+30
+Waan Qalbii Qara Taasisan
+31
+Qoricha Doofummaa
+32
+Qoricha Fedhii Nyaataa Dhabuu
+Barruulee Dubbisi
+Kutaa 01 / Qoricha Uumamaa
+
+Haba-suudaa
+1. Sirna Hargansaa fi Utallaa
+Utallaa: Yoo Haba-suudaan waxalamtee (fried), uffata killaasii keessatti hidhamtee funyaaniin urguffatan (inhale), utallaa gogaa fi qorra irraa dhufuuf furmaata dha.
+
+Asmii fi Hargansaa: Qoonqoo fi qoma keessaa dhibee itti citu (mucus) qulqulleessuun hargansaa ni salphisti.
+
+2. Sirna Nyaataa fi Summii
+Garaa Kaasaa fi Garaa Gubaa: Garaa keessaa dhuufa ykn dhuunfuu (gas) ni balleessiti.
+
+Rirma Garaa: Yoo dammaa fi bishaan ho'aan dhugame, rirma garaa (worms) ni ajjeesti.
+
+Dhagaa Kalee: Yoo dammaa fi bishaan ho'aa waliin dhugame, dhagaa kalee fi afuuffee keessatti uumamu ni bulbullee baasti.
+
+3. Dhukkubbii Qaama Alaa
+Mataa Bowwuu: Yoo bifa dibataatiin ykn vaayinaagira (vinegar) waliin adda irratti dibatan, mataa bowwuu qorra irraa dhufuuf qoricha.
+
+Ilkaan: Yoo bishaan vaayinaagiraa keessatti affeelamee ittiin kofalani (rinse), dhukkubbii ilkaanii ni hir'isti.
+
+Barasii fi Dhukkuba Gogaa: Yoo ashaaraa (vinegar) waliin walitti makuun gogaa irratti dibatan, barasii fi shiftoo gogaa tokko tokkoof ni gargaarti.
+
+4. Faayidaalee Biroo
+Garaacha: Nyaata bullaa'uu didu bulleessuuf ni gargaarti.
+
+Dhibee Dubartootaa: Marsaa xurii (menstruation) sirreessuu fi aannan harmaa dubartootaaf dabaluu irratti shoora ni qabdi.
+
+Qajeelfama Ibnul Qayyim Dabarse
+Ibnul Qayyim akka jedhutti, Haba-suudaan miira "Ho'aa fi Gogaa" waan qabduuf, dhibeewwan jiidha (moisture) fi qorra irraa dhufan qolachuuf baay'ee gaariidha.
+
+Yaadachiisa: Akka ammayyaatti, Haba-suudaa fayyadamuu yoo barbaadde, mudaa (dosage) ishee gahaa ta'e qofa fayyadami. Yoo dhibee biroo qabaatte ogeessa gorsa gaafachuun gaariidha.
+Qajeelfama Fayyaa
+Kutaa 04 / Qoricha Uumamaa
+
+Qullubbii Adii
+Ibnul Qayyim akka jedhutti, Qullubbii Adiin qoricha baay'ee jabaa ho'aa fi gogaa ta'e dha. Faayidaalee isaa keessaa:
+
+Summii Ittisuu: Nama summiin qabameef, akkasumas nama humni bofaa ykn aqraabaa waraaneef dhibee ni qolata; bifa dibataatiinis ni ta'a.
+
+Dhuunfuu fi Koflaa: Dhuunfuu (gas) garaa keessaa ni balleessa, koflaa (phlegm) ni kutata, garaacha keessaa jiidha badala ta'e ni qulqulleessa.
+
+Fincaan fi Marsaa Xurii: Fincaan dhangalaasuu irratti humna guddaa qaba; dubartoota xuriin dhiigaa jalaa citeefis ni gargaara.
+
+Qomaa fi Quufaa: Yoo affeelamee bishaan isaa dhugame, quufaa (cough) yeroo dheeraa fi dhukkubbii qomaa qorra irraa dhufuuf furmaata.
+
+Madaa fi Gogaa: Yoo damma waliin makuun barasii (vitiligo) irratti dibatan ni fayyada; dhiitama qaamaa ni bulbullee baasa.
+
+Ilkaan: Yoo ashaaraa (vinegar) waliin affeelamee ittiin kofalani, dhukkubbii ilkaanii ni hir'isa.
+
+تنبيه ابن القيم (Yaadachiisa)
+Ibnul Qayyim akka jedhutti, yoo baay'inaan nyaatame mata-bowwuu fiduu fi ija miidhuu danda'a. Foolii isaa balleessuuf garuu affeelanii (cooked) nyaachuun gaariidha.
+Qajeelfama Fayyaa
+Kutaa 05 / Qoricha Uumamaa
+
+Hulubataa
+1. Sirna Hargansaa fi Qomaa:
+Hulubanni bishaan keessatti affeelamee yoo dhugame, Quufaa (cough) fi Asmii ni salphisti.
+Qomaa fi dhuunshoo (lungs) keessaa dhibee furriifi koflaa (phlegm) furmisiisanii baasuuf baay'ee gargaarti.
+
+2. Sirna Nyaataa fi Garaa:
+Dhukkubbii garaa fi mar'ummaanii ni qabbaneessiti.
+Garaa gogaa ni laaffisti (Laxative), dhuunfuu fi gubaa garaa keessaa ni balleessiti.
+
+3. Faayidaa Dubartootaaf:
+Dubartoota hoosisaniif aannan harmaa akka baay'atu gochuu irratti humna guddaa qabdi.
+Marsaa xurii (menstruation) dubartootaa ni sirreessiti.
+
+4. Dhukkubbii Alaa fi Gogaa:
+Mata-biraa ykn dhiitama qaama alaa irratti yoo dibatan (Lidhuu/Poultice), dhiitama ni bulbullee baasti.
+Rifeensaaf yoo bishaan isheetiin mataa dhiqatan, rifeensa ni jabeessiti, qola mataa (dandruff) ni balleessiti.
+
+5. Dhukkuboota Biroo:
+Dhukkubii Bawaasiraa (Hemorrhoids) qabaniif, bishaan ishee keessa taa'uun (sitting bath) dhukkubbii ni hir'isa.
+Madaa fi dhiitama kalee fi madiitti uumamuuf qoricha gaariidha.
+
+Akkaataa Itti Fayyadamaa (Akka Heddii Nabiyyiitti):
+Dhuguuf: Bulee ishee bishaan keessatti affeelanii bishaanicha dammaan makuun dhuguu.
+Dibachuuf: Hulubaa daakamte damma ykn ashaaraa (vinegar) waliin makuun bakka dhiitamaa ykn madaa irratti dibachuu.
+Nyaataaf: Akkuma jirtutti nyaata waliin makuu.
+Mammaaksa Ogeeyyii Durii:
+"Namoonni otoo faayidaa Hulubata keessa jiru beekanii, otoo gatii warqeetiin bitamteellee ni bitatu turan."
+
+Hulubanni ho'aa fi jiidha waan qabduuf, keessumaa dhibeewwan qorraa fi gogaa irraa dhufaniif furmaata guddaadha.
+Qajeelfama Fayyaa
+Kutaa 06 / Qoricha Uumamaa
+
+Qoonxar/Talbaa
+Ibnul Qayyim kitaaba isaa "Xibb-un-Nabawii" keessatti sanyiin Qoonxar/Talbaa: ho'aa fi jiidhaa akka ta'ee fi faayidaa guddaa akka qabu ibseera.
+
+Quufaa fi Qoma: Quufaa yeroo dheeraa ni qabbaneessa; dhukkubbii qomaa fi sombaaf ni gargaara.
+
+Kalee fi Fincaan: Fincaan ni dhangalaasa; yoo dammaan makamee dhugame dhagaa kalee fi afuuffee fincaanii keessatti uumamu bulbullee baasuuf gargaara.
+
+Dhiitama fi Gogaa: Yoo ashaaraa (vinegar) waliin makuun bakka dhiitamaa irratti dibatan, dhiitama ni bulbullee baasa; gogaa ni qulqulleessa, rifeensa ni fooyyessa, qola mataas (dandruff) ni balleessa.
+
+Akkaataa Itti Fayyadamaa:
+Ibnul Qayyim akka jedhutti, sanyiin killaasii yoo waxalame (roasted) garaa hidha (constipation), yoo akkuma jirutti (dhuunshaan) fayyadaman immoo garaa laaffisaa fi xurii garaa keessaa baasuuf gargaara.
+
+Kutaa 07 / Qoricha Uumamaa
+
+Shunkurtaa Diimaa
+Ibnul Qayyim akka jedhutti shunkurtaa diimaan ho'aa fi jiidha dabalataa kan qabduudha. Faayidaalee ishee keessaa:
+
+Summii Ittisuu: Bishaan bakkaa bakkatti jijjiiramuun dhufe ykn qilleensa summaa'aa irraa dhukkuba dhufu ni ittisa.
+
+Sirna Nyaataa: Fedhii nyaataa ni dabala, garaacha ni jabeessa, koflaa (phlegm) ni kutata, akkasumas jiidha garaacha keessa jiru ni qulqulleessa.
+
+Midihaa fi Ijaa: Yoo damma waliin makuun akka kuulaatti ija dibatan, bishaan ija keessa jiruuf gaariidha. Yoo barasii (vitiligo) irratti dibatan ni fayyada.
+
+Fincaan fi Humna Qaamaa: Fincaan ni dhangalaasa, humna saal-quunnamtii ni dabala, marsaa xurii dubartootaas ni sirreessa.
+
+Madaa fi Shiftoo: Yoo soogidda waliin makuun godaannisa ykn kormatti (warts) irratti dibatan, buqqisee baasa.
+
+Gorsa Nama Karaa Deemuuf: Namni gara biyya biraa deemu tokko, dhibee naannoo sanaa fi bishaan jijjiirame irraa of eeguuf shunkurtaa diimaa nyaachuu qaba jedha.
+
+Yaadachiisa Ibnul Qayyim (تنبيه مهم):
+Ibnul Qayyim akka jedhutti, namni shunkurtaa nyaate yoo gara masjiidaa deemu foolii ishee balleessuuf baala Sududaa (Rue) nyaachuu ykn shunkurtaacha affeelanii (cooked) nyaachuun foolii ishee akka hir'isu ibseera.
+Qajeelfama Fayyaa
+Kutaa 08 / Qoricha Uumamaa
+
+Rummaanaa
+Ibnul Qayyim akka jedhutti, Rummaanichi akaakuu lama (mi'aawaa fi dhangaggaa'aa) qaba, tokkoon tokkoon isaaniis faayidaa addaa qabu:
+
+Rummaana Mi'aawaa: Ho'aa fi jiidhaa dha; garaachaaf baay'ee gaariidha, ni jabeessa. Qoonqoo, qomaa fi sombaaf ni fayyada, quufaas ni qabbaneessa.
+
+Rummaana Dhangaggaa'aa: Qorraa fi gogaadha; garaacha jabeessuu irratti humna guddaa qaba. Fincaan ni dhangalaasa, finniisa (bile) ni qabbaneessa, garaa kaasaa ni dhaaba, akkasumas haqqee ni ittisa.
+
+Irgii fi Afaan: Yoo bishaan qola isaa affeelamee ittiin kofalani (rinse), irgii dhiigu ni jabeessa.
+
+Madaa: Qolli rummaanaa yoo akka dibataatti (ضماد) fayyadaman, madaa hamaa ta'e fayyisuuf gargaara.
+
+Yaadachiisa: Rummaana yoo hunda isaa (lahee keessaa waliin) nyaatan, garaacha ni jabeessa (tanning effect), nyaata bulleessuufis ni gargaara.
+
+قاعدة ابن القيم في الرمان / Qajeelfama Ibnul Qayyim
+الرمان الحلو يوافق الصدر، والحامض يوافق المعدة الملتهبة.
+
+[Afaan Oromoo]: Rummaanni mi'aawaan qomaaf deeggarsa kenna, inni dhangaggaa'aan immoo garaacha gubatuuf (inflammation) furmaata.
+Qajeelfama Fayyaa
+Kutaa 09 / Qoricha Uumamaa
+
+Timiraa
+Ibnul Qayyim akka jedhutti, Timirri nyaata qofa otoo hin taane qoricha hundaa oliti. Faayidaalee isaa keessaa:
+
+Tiruun fi Garaacha: Timirri tiruu fi garaacha ni jabeessa, nyaata qaamaaf humna guddaa kennuudha.
+
+Summii fi Sihrii Ittisuu: Hadiisa Nabiyyii (scw) irratti hundaa'uun, Timirri (keessummaa Ajwaan) summii fi ija-budaa ykn sihrii irraa nama eega jedha.
+
+Rirma Garaa: Yoo ganama otoo nyaata biraa hin nyaatin (on empty stomach) nyaatame, rirma garaa (worms) ni ajjeesa.
+
+Qomaa fi Qoonqoo: Dhukkubbii qomaa fi sombaaf gaariidha, keessumaa namoota naannoo ho'aa jiraataniif humna guddaa kenna.
+
+Timira Jiidhaa (Ruxab): Garaacha qorraan miidhame ni jabeessa, humna saal-quunnamtii ni dabala, qaamas ni gabbisa.
+
+Summaa'ina Ittisuu: Qaama keessatti xuriin akka hin uumamne ykn nyaanni garaa keessatti akka hin kuffisne ni ittisa.
+
+نصيحة ابن القيم في أكل التمر / Gorsa Ibnul Qayyim:
+هو فاكهة وغذاء ودواء وحلوى، وهو من أفضل ما يُفطر عليه الصائم ليرد إلى الكبد قواها.
+
+[Afaan Oromoo]: Timirri fuduraadha, nyaata, qorichas, akkasumas mi'aawaadha. Namni soomanu tokko yeroo furu Timiraan yoo eegale, tiruun isaa humna ishee ariitiin akka deeffattu gochuuf filatamaa dha.
+Qajeelfama Fayyaa
+Kutaa 10 / Qoricha Uumamaa
+
+Tamiirul Hindii
+Ibnul Qayyim akka jedhutti, Tamarindii (Tamiirul Hindii) qorraa fi gogaadha. Faayidaalee isaa keessaa:
+
+Dhibee Finniisaa (Bile): Ho'a finniisaa (bile) garmalee qaama keessatti ka'u ni qabbaneessa.
+
+Garaa Laaffisuu: Garaa laaffisuu (laxative) irratti shoora ni qaba, xurii ho'aa garaa keessa jiru gargaaraa baasuuf gargaara.
+
+Dheebuu fi Haqqee: Dheebuu jabaa ni qabbaneessa, haqqee fi miira ol-garagalchu (nausea) ni ittisa.
+
+Dhiiga Qulqulleessuu: Dhiiga ho'a irraa qabbaneessuu fi xurii qaamaa keessaa baasuuf baay'ee gaariidha, keessummaa namoota biyya ho'aa jiraataniif.
+
+Dhukkuba Ho'aa (Fever): Namoota dhibee ho'aa ykn qandhoo qabaniif, dhibee san qabbaneessuuf qoricha gaariidha.
+
+قاعدة الاستعمال / Qajeelfama Itti Fayyadamaa
+يُفضل شربه نقعاً في الماء مع قليل من السكر أو العسل لتعديل حموضته، وهو مفيد جداً في فصل الصيف.
+
+[Afaan Oromoo]: Akka inni mi'aawuuf bishaan keessa bulchanii (soaking) damma ykn sukkaara xiqqoo waliin dhuguun filatamaadha. Keessumaa waqtii gannaa (ho'a jabaa) keessatti baay'ee barbaachisaadha.
+Qajeelfama Fayyaa
+Kutaa 11 / Qoricha Uumamaa
+
+Jinjiibilaa
+Ibnul Qayyim akka jedhutti, Jinjiibiliin ho'aa fi gogaadha. Akka Kuraana keessatti ibsametti, nyaata warra jannataati. Faayidaalee isaa keessaa:
+
+Garaacha fi Tiruu: Garaachaa fi tiruu ni ho'isa, nyaata bulleessuu irratti shoora guddaa qaba. Tiruu qorraan miidhamteef qoricha.
+
+Dhuunfuu fi Koflaa: Dhuunfuu (gas) garaa keessaa ni balleessa, koflaa (phlegm) nyaata qorraa fi fuduraa irraa uumamu ni kutata.
+
+Yaadannoo fi Sammuu: Sammuu keessaa jiidha badaa qulqulleessuun yaadannoo ni jabeessa, irraanfannoo ni hir'isa.
+
+Fayyaa Ijaa: Yoo damma waliin makuun akka kuulaatti ija dibatan, dimimmisa ijaa ni qulqulleessa.
+
+Humna Qaamaa: Humna saal-quunnamtii ni dabala, qaama ni kakaasa.
+
+Summii Ittisuu: Summii dhibeewwan qorra irraa dhufan ni ittisa.
+
+نصيحة الاستعمال / Gorsa Itti Fayyadamaa
+ذكر أن الزنجبيل إذا خلط مع العسل والماء الحار، كان أنفع في طرد البلغم وتنظيف الصدر.
+
+[Afaan Oromoo]: Ibnul Qayyim akka jedhutti, Jinjiibila dammaa fi bishaan ho'aa waliin makuun dhuguun, koflaa qulqulleessuu fi qoma jabeessuuf hunda caala.
+Qajeelfama Fayyaa
+Kutaa 18 / Qoricha Uumamaa
+
+Wantoota Fayyaa Miidhan
+Ibnul Qayyim akka jedhutti, wantoonni Fayyaa Miidhan (Health Spoilers) fi dandeettii qaamaa dadhabsiisan baay'eedha. Isaan keessaa warra ijoo:
+
+Nyaata Irratti Nyaachuu: Otoo inni duraa hin bullaayin kan biraa nyaachuun garaacha jabeessee miidha, madda dhibeeti.
+
+Garmalee Quufu: Garaa guuttatanii nyaachuun qaama ni ulfaatessa, sammuu ni doofumsa, hafuura baafachuus ni dhiphisa.
+
+Nyaata Irratti Bishaan Dhuguu: Keessumaa bishaan qorraa yeroo nyaataa dhuguun ho'a nyaata bulleessuuf barbaachisu waan qabbaneessuuf, nyaata akka hin bullaayne godha.
+
+Guyyaa Rafuu: Hirribni guyyaa (keessummaa ganama ykn galgala booda) qaama ni dadhabsiisa, bifa fuulaa ni balleessa, akkasumas dhibee jiidha irraa dhufu fida.
+
+Xurii Qaamaa Dhowwachuu: Fincaan, dhuunfuu fi xurii garaa dhowwachuun summiin gara dhiigaatti akka deebi'u gochuun kalee fi mar'ummaan ni miidha.
+
+Saal-quunnamtii Baay'isuu: Humna qaamaa ni fixi, ija ni dadhabsiisa, akkasumas qaama ni huqqisa (premature aging).
+
+Dhiphuu fi Aarii Jabaa: Aariin, yaaddoon fi gaddi jabaan onnee ni dadhabsiisu, dandeettii qaamaa (immunity) ni cabsu.
+
+قاعدة ذهبية في التجنب / Qajeelfama Of-qussuu
+حمية الجسم عن الفضولات، وحمية الروح عن الآفات، هما جماع الصحة.
+
+Qaama xurii irraa eeguu fi hafuura (soul) dhibee irraa eeguun, walitti qabaa fayyaati.
+Qajeelfama Fayyaa
+Qabiyyeen hafe asitti ittifufla...
+Icciitiin barreeffamichaa hundi akkuma jirtutti tura.
+
+© 2024 Xibbunnabawi Digital Archive
+Oromia, Ethiopia
